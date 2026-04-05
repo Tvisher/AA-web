19db9ab8b5ef65ae48acf5e4bb96170d6d17d5d4
Tvisher <tvisherfine@gmail.com>
2026-04-05 17:59:59 +0500

    +

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.cards-slider', {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    //   breakpoints: {
+    //     // when window width is >= 320px
+    //     320: {
+    //       slidesPerView: 2,
+    //       spaceBetween: 20
+    //     },
+    //     // when window width is >= 480px
+    //     480: {
+    //       slidesPerView: 3,
+    //       spaceBetween: 30
+    //     },
+    //     // when window width is >= 640px
+    //     640: {
+    //       slidesPerView: 4,
+    //       spaceBetween: 40
+    //     }
+    //   }
+})

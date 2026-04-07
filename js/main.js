75057fc8e6ba41e135d8e911d2f8eeeca87ffd9a
Tvisher <tvisherfine@gmail.com>
2026-04-07 20:55:01 +0500

    +

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.cards-slider', {
+const cardsSlider = new Swiper('.cards-slider', {
     slidesPerView: 'auto',
     spaceBetween: 20,
     pagination: {
@@ -26,4 +26,77 @@ const swiper = new Swiper('.cards-slider', {
     //       spaceBetween: 40
     //     }
     //   }
+})
+
+
+
+
+
+const reviewTexts = new Swiper('.reviews-second-slider', {
+    slidesPerView: 1,
+    allowTouchMove: 0,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+});
+
+
+const reviewsMainSlider = new Swiper('.reviews-main-slider', {
+    slidesPerView: 1,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    pagination: {
+        el: '.reviews-main-slider .swiper-pagination',
+        type: 'bullets',
+    },
+    navigation: {
+        nextEl: ' .reviews-main-slider .swiper-button-next',
+        prevEl: ' .reviews-main-slider .swiper-button-prev',
+    },
+    on: {
+        activeIndexChange(slider) {
+            reviewTexts.slideTo(slider.activeIndex);
+        }
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const {
+        OverlayScrollbars,
+        ScrollbarsHidingPlugin,
+        SizeObserverPlugin,
+        ClickScrollPlugin
+    } = OverlayScrollbarsGlobal;
+
+    const textes = document.querySelectorAll('.review-text__inner')
+    textes.forEach(item => {
+        const osInstance = OverlayScrollbars(item, {
+            overflow: {
+                x: 'hidden',
+            },
+            scrollbars: {
+                dragScroll: true,
+                clickScroll: false,
+                pointers: ['mouse', 'touch', 'pen'],
+            },
+        }, {
+            scroll: (instance, event) => {
+                const { viewport } = instance.elements();
+                const { scrollTop, scrollHeight, clientHeight } = viewport;
+                const maxScroll = scrollHeight - clientHeight;
+                const progress = maxScroll > 0 ? (scrollTop / maxScroll) * 100 : 0;
+                if (progress.toFixed(2) >= 90) {
+                    item.classList.add('end')
+                } else {
+                    item.classList.remove('end')
+
+                }
+            }
+        });
+    })
+    // Инициализируем
+
 })

21d451ee3f5edaa62020922025f5e7e5f5cbe1e6
Tvisher <tvisherfine@gmail.com>
2026-04-12 17:22:06 +0500

    !

--- js/main.js
+++ js/main.js
@@ -147,10 +147,10 @@ const articleSectionSlider = new Swiper('.article-section__slider', {
     },
 });
 
-
+const isMobile = window.innerWidth <= 768;
 AOS.init({
     // Global settings:
-    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    disable: isMobile, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
     startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
     initClassName: 'aos-init', // class applied after initialization
     animatedClassName: 'aos-animate', // class applied on animation
@@ -165,7 +165,6 @@ AOS.init({
     once: true, // whether animation should happen only once - while scrolling down
     mirror: false, // whether elements should animate out while scrolling past them
     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
 });
 
 
